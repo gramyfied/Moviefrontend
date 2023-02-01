a6663cd1073ebe87a8b204b6118fbb8de4165877
gramyfied <ousmane.sissoko@sundiata-tech.com>
2023-02-01 13:48:47 +0100

    Mymovies deploiement

---
+++ components/Home.js
@@ -0,0 +1,75 @@
+import { useState,useEffect } from 'react';
+import { Popover, Button } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import Movie from './Movie';
+import 'antd/dist/antd.css';
+import styles from '../styles/Home.module.css';
+
+const API = '86a36a565c90905e678f3403efe73c0b';
+
+function Home() {
+  const [likedMovies, setLikedMovies] = useState([]);
+  const [moviesData, setMoviesData] = useState([]);
+
+  // Liked movies (inverse data flow)
+  const updateLikedMovies = (movieTitle) => {
+    if (likedMovies.find(movie => movie === movieTitle)) {
+      setLikedMovies(likedMovies.filter(movie => movie !== movieTitle));
+    } else {
+      setLikedMovies([...likedMovies, movieTitle]);
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API}`)
+      .then(res => res.json())
+      .then(data => {
+        console .log(data.results)
+        setMoviesData(data.results)
+      })
+      .catch(console.error);
+  }, [])
+
+  const likedMoviesPopover = likedMovies.map((data, i) => {
+    return (
+      <div key={i} className={styles.likedMoviesContainer}>
+        <span className="likedMovie">{data}</span>
+        <FontAwesomeIcon icon={faCircleXmark} onClick={() => updateLikedMovies(data)} className={styles.crossIcon} />
+      </div>
+    );
+  });
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>
+      {likedMoviesPopover}
+    </div>
+  );
+
+  const movies = Object.values(moviesData).map((data, i) => {
+    const imageUrl = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+    const voteAverage = data.voteAverage;
+    const voteCount = data.voteCount;
+    const isLiked = likedMovies.some(movie => movie === data.title);
+    return <Movie key={i} updateLikedMovies={updateLikedMovies} isLiked={isLiked} title={data.title} overview={data.overview} poster={imageUrl} voteAverage={voteAverage} voteCount={voteCount} />;
+  });
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.logocontainer}>
+          <img src="logo.png" alt="Logo" />
+          <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
+        </div>
+        <Popover title="Liked movies" content={popoverContent} className={styles.popover} trigger="click">
+          <Button>♥ {likedMovies.length} movie(s)</Button>
+        </Popover>
+      </div>
+      <div className={styles.title}>LAST RELEASES</div>
+      <div className={styles.moviesContainer}>
+        {movies}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
